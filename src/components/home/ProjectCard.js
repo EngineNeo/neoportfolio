44c1972a08f6161f6b310ccc228b8c5cf3d4b41e
EngineNeo <neomaralit@gmail.com>
2024-01-17 02:31:48 -0500

    Added projects to section

--- src/components/home/ProjectCard.js
+++ src/components/home/ProjectCard.js
@@ -1,43 +1,87 @@
 import React from 'react';
 import Card from '@mui/material/Card';
-import { withStyles } from '@mui/styles';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import LanguageIcon from '@mui/icons-material/Language'; // Import for web icon
+import { withStyles } from '@mui/styles';
 
 const styles = (theme) => ({
   card: {
-    height: '500px',
-    width: '300px',
+    display: 'flex',
+    flexDirection: 'column',
+    height: '600px',
+    width: '400px',
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  cardContent: {
+    flexGrow: 1,
+    overflow: 'auto',
+  },
+  cardActions: {
+    justifyContent: 'flex-start',
+    padding: theme.spacing(1),
+    backgroundColor: 'transparent',
+  },
+  title: {
+    color: theme.palette.common.white,
+  },
+  description: {
+    color: theme.palette.common.white,
+  },
+  icon: {
+    color: theme.palette.grey[400],
+    fontSize: '1.5em',
   },
 });
 
+
 const ProjectCard = (props) => {
-  const { classes, image, title, description, githubLink } = props;
+  const { classes, image, title, description, githubLink, websiteLink } = props;
+
+  const renderGitHubLinks = () => {
+    if (Array.isArray(githubLink)) {
+      return githubLink.map((link, index) => (
+        <IconButton key={index} aria-label="github link" onClick={() => window.open(link, '_blank')}>
+          <GitHubIcon className={classes.icon} />
+        </IconButton>
+      ));
+    } else {
+      return (
+        <IconButton aria-label="github link" onClick={() => window.open(githubLink, '_blank')}>
+          <GitHubIcon className={classes.icon} />
+        </IconButton>
+      );
+    }
+  };
 
   return (
     <Card className={classes.card}>
       <CardMedia
         component="img"
-        height="140"
+        height="200"
         image={image}
         alt={title}
       />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+      <CardContent className={classes.cardContent}>
+        <Typography gutterBottom variant="h4" component="div" className={classes.title}>
           {title}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="body2" className={classes.description}>
           {description}
         </Typography>
       </CardContent>
-      <CardActions>
-        <IconButton aria-label="github link" onClick={() => window.open(githubLink, '_blank')}>
-          <GitHubIcon />
-        </IconButton>
+      <CardActions className={classes.cardActions}>
+        {renderGitHubLinks()}
+        {websiteLink && (
+          <IconButton aria-label="website link" onClick={() => window.open(websiteLink, '_blank')}>
+            <LanguageIcon className={classes.icon} />
+          </IconButton>
+        )}
       </CardActions>
     </Card>
   );
--- src/components/home/ProjectSection.js
+++ src/components/home/ProjectSection.js
@@ -5,22 +5,24 @@ import { withStyles } from '@mui/styles';
 
 const projects = [
   {
-    image: 'path/to/image1.jpg',
-    title: 'Project One',
-    description: 'This is a description for Project One.',
-    githubLink: 'https://github.com/user/project-one'
+    image: 'items/FitConnect.png',
+    title: 'FitConnect',
+    description: 'FitConnect, a comprehensive fitness application, is my latest full-stack development project, completed with a talented team. As the lead developer, I integrated React for a dynamic user interface, Django for robust back-end functionality, and MySQL for efficient data management. This app allows users to meticulously track their exercises, weight, mood, and calorie intake. It features a sophisticated user interface and a unique platform for coaches to interact directly with users, enhancing the fitness journey for different user types.',
+    githubLink: ['https://github.com/EngineNeo/FitConnect-Frontend',
+                 'https://github.com/EngineNeo/FitConnect-Backend']
   },
   {
-    image: 'path/to/image2.jpg',
-    title: 'Project Two',
-    description: 'This is a description for Project Two.',
+    image: 'items/Options.png',
+    title: 'Options Pricing Application',
+    description: 'An advanced options pricing application using Streamlit, harnessing the power of Python\'s robust libraries. This application utilizes NumPy for numerical computations, Pandas for data handling, and Matplotlib for visualizations. I\'ve integrated QuantLib for complex financial calculations and Scikit- learn for predictive modeling. The user - friendly Streamlit interface brings this complex functionality to life, making options pricing analysis both accessible and comprehensive for financial professionals and enthusiasts.',
     githubLink: 'https://github.com/user/project-two'
   },
   {
-    image: 'path/to/image2.jpg',
-    title: 'Project Three',
-    description: 'This is a description for Project Three.',
-    githubLink: 'https://github.com/user/project-Three'
+    image: 'items/OliviaChen.png',
+    title: 'Portfolio',
+    description: 'I designed an innovative portfolio website using React for a dynamic, responsive user interface, and Firebase for robust back-end functionality. The site features full CRUD (Create, Read, Update, Delete) capabilities, allowing users to effortlessly manage their content. React\'s component- based architecture enhances the user experience with interactive elements, while Firebase provides a real - time database, authentication, and hosting.This combination ensures a seamless, intuitive experience for users, showcasing my skills in modern web development technologies."',
+    githubLink: 'https://github.com/user/project-Three',
+    websiteLink: 'https://oliviachen.art/'
   },
 ];
 
@@ -71,7 +73,7 @@ const ProjectSection = (props) => {
   return (
     <Fragment>
       <div className={classes.wrapper}>
-        <Grid container spacing={2} className={classes.gridContainer}>
+        <Grid container spacing={1} className={classes.gridContainer}>
           {projects.map((project, index) => (
             <Grid item key={index} xs={12} sm={6} md={4} className={classes.gridItem}>
               <ProjectCard {...project} />
