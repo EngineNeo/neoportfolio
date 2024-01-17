215d0baad815341a8b9993d4a6fc49624dcfd42a
EngineNeo <neomaralit@gmail.com>
2024-01-17 02:34:07 -0500

    Updates

---
+++ precache-manifest.7dd23f4dd3c5aecd9e3741e61e77edf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd1fefb16355aeef585a",
+    "url": "/neoportfolio/static/js/main.e2e800b0.chunk.js"
+  },
+  {
+    "revision": "dae5d0eb4eaf59455b84",
+    "url": "/neoportfolio/static/js/runtime~main.b396894f.js"
+  },
+  {
+    "revision": "3ab084a7273356653047",
+    "url": "/neoportfolio/static/css/2.5b86d0c0.chunk.css"
+  },
+  {
+    "revision": "3ab084a7273356653047",
+    "url": "/neoportfolio/static/js/2.b432fb86.chunk.js"
+  },
+  {
+    "revision": "d475c24a0581ff17cc31",
+    "url": "/neoportfolio/static/js/3.f1f78b68.chunk.js"
+  },
+  {
+    "revision": "49be529c1ef590f59044",
+    "url": "/neoportfolio/static/js/4.233eb635.chunk.js"
+  },
+  {
+    "revision": "cfdfea35c6e9fa4c53595d9fc32f1d57",
+    "url": "/neoportfolio/index.html"
+  }
+];
